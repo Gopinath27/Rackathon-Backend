ed33d32fe3d31f10de42c1dcbd143fdb734c1921
Gopinath S <33867929+Gopinath27@users.noreply.github.com>
2021-01-11 17:25:26 +0530

    Update LoginController.java

--- ProductRecall/src/main/java/com/rackathon/controller/LoginController.java
+++ ProductRecall/src/main/java/com/rackathon/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.rackathon.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
