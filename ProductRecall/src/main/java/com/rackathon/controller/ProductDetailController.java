434b75a09849aa70f1d52de7ba8e54bfef367290
Gopinath S <33867929+Gopinath27@users.noreply.github.com>
2021-01-18 21:01:15 +0530

    Update ProductDetailController.java

--- ProductRecall/src/main/java/com/rackathon/controller/ProductDetailController.java
+++ ProductRecall/src/main/java/com/rackathon/controller/ProductDetailController.java
@@ -1,23 +1,10 @@
 package com.rackathon.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
-import java.util.Map;
 
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-
-import org.apache.http.HttpException;
-import org.json.JSONArray;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -52,7 +39,7 @@ public class ProductDetailController {
 	 * }
 	 */
 	@GetMapping(value="/fetchproduct")
-	public List fetchProduct(@RequestParam(name = "storeid",required = false) Integer storeid, @RequestParam(name = "productid",required = false) Integer productid) throws HttpException {
+	public List fetchProduct(@RequestParam(name = "storeid",required = false) Integer storeid, @RequestParam(name = "productid",required = false) Integer productid) {
 		// Fetch all products based on Store ID
 		ProductDetailDao productDetailDao = new ProductDetailDao();
 		
