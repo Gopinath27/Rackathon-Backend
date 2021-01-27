f3481b37337e399788f615c0825841d323540ae5
Gopinath S <33867929+Gopinath27@users.noreply.github.com>
2021-01-27 15:49:52 +0530

    Update ProductReturn.java

--- ProductRecall/src/main/java/com/rackathon/controller/ProductReturn.java
+++ ProductRecall/src/main/java/com/rackathon/controller/ProductReturn.java
@@ -2,6 +2,7 @@ package com.rackathon.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rackathon.dao.ProductDetailDao;
 import com.rackathon.dao.ProductReturnDao;
 
+@CrossOrigin(origins= {"http://localhost:3000","https://productrecall.azurewebsites.net","http://productrecall.azurewebsites.net"} )
 @RestController
-public class ProductReturn {
+public class ProductReturnController {
 	
 	@GetMapping(value="/fetchissueproduct")
 	public List fetchIssueProduct(@RequestParam(name = "productid") int productID) {
