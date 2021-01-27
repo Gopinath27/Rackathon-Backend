ef38bf0d1e94d3d5be02a50dfbda3b9928e1755d
Gopinath S <33867929+Gopinath27@users.noreply.github.com>
2021-01-27 16:04:35 +0530

    Update ProductRecallApplication.java

--- ProductRecall/src/main/java/com/rackathon/productrecall/ProductRecallApplication.java
+++ ProductRecall/src/main/java/com/rackathon/productrecall/ProductRecallApplication.java
@@ -1,8 +1,11 @@
 package com.rackathon.productrecall;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductRecallApplication {
 
 	public static void main(String[] args) {
+		System.out.println("From JAVA OPTS URL :: "+System.getProperty("url"));
 		SpringApplication.run(ProductRecallApplication.class, args);
-	}
+		}
 
 @RestController
 class Login{
 	@GetMapping("/")
 	public String log() {
-		return "Welcome to ProductRecall REST API";
+		return "<H1>Welcome to ProductRecall REST API</H1>";
 	}
 }
 }
