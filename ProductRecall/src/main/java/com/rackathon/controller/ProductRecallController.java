4d1ee385e68cd7c4cf7a0b2dd9443f071a1b845c
Gopinath S <33867929+Gopinath27@users.noreply.github.com>
2021-01-27 15:48:51 +0530

    Update ProductRecallController.java

--- ProductRecall/src/main/java/com/rackathon/controller/ProductRecallController.java
+++ ProductRecall/src/main/java/com/rackathon/controller/ProductRecallController.java
@@ -2,20 +2,29 @@ package com.rackathon.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rackathon.dao.ProductRecallDao;
 
+@CrossOrigin(origins= {"http://localhost:3000","https://productrecall.azurewebsites.net","http://productrecall.azurewebsites.net"} )
 @RestController
 public class ProductRecallController {
 
 	@RequestMapping(value="/productRecall")
-	public List productRecall(@RequestParam(name = "storeid") int storeID, @RequestParam(name = "productid") int productID, @RequestParam(name = "listmfg") List<Integer> mfgList) {
+	public ResponseEntity<String> productRecall(@RequestParam(name = "storeid") int storeID, @RequestParam(name = "productid") int productID, @RequestParam(name = "listbatch") List<String> batchList) {
 		
 		ProductRecallDao productRecallDao = new ProductRecallDao();
-		return productRecallDao.productRecall(storeID, productID, mfgList);
+		if(productRecallDao.productRecall(storeID, productID, batchList)) {
+			return new ResponseEntity<String>(HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
 		
 	}
 	
